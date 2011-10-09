5a1d04714393f7af7a449b48bcacb0fe33b1df31
Raju Gandhi <raju.gandhi+github@gmail.com>
2011-10-09 17:40:11 -0400

    Introduce application helper to set title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+  def title
+    base_title = "Ruby On Rails Tutorial Sample App"
+    if @page_name.nil?
+      base_title
+    else
+      "#{base_title} | #{@page_name}"
+    end
+  end
 end
