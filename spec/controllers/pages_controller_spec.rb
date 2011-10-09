cfaffdea9293f2d3dbe532afa5b3337f349859e5
Raju Gandhi <raju.gandhi+github@gmail.com>
2011-10-09 14:47:07 -0400

    Added tests to ensure body is not empty

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -15,6 +15,11 @@ describe PagesController do
       response.should have_selector("title",
                                     content: "Ruby On Rails Tutorial Sample App | Home")
     end
+
+    it "should have a non-blank body" do
+      get 'home'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 
   describe "GET 'contact'" do
@@ -26,6 +31,10 @@ describe PagesController do
       get 'contact'
       response.should have_selector("title",
                                     content: "Ruby On Rails Tutorial Sample App | Contact")
+    end
+    it "should have a non-blank body" do
+      get 'contact'
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -40,6 +49,10 @@ describe PagesController do
       response.should have_selector("title",
                                     content: "Ruby On Rails Tutorial Sample App | About")
     end
-    
+
+    it "should have a non-blank body" do
+      get 'about'
+      response.body.should_not =~ /<body>\s*<\/body>/
+    end
   end
 end
