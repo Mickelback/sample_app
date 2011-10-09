aaf96f5d19b6e9ee87d605c73d3c110f955267d2
Raju Gandhi <raju.gandhi+github@gmail.com>
2011-10-09 16:33:53 -0400

    Refactored title to use page level variable

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @page_name = "Home"
   end
 
   def contact
+    @page_name = "Contact"
   end
 
   def about
+    @page_name = "About"
   end
 end
